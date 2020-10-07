7728384a240af0a92bcb40620ea805a3f47ddbd1
Peter <peternorton@berkeley.edu>
2020-10-06 19:26:43 -0700

    changes

--- backend/humboldt/settings/base.py
+++ backend/humboldt/settings/base.py
@@ -66,12 +66,6 @@ WSGI_APPLICATION = 'humboldt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
--- backend/humboldt/settings/development.py
+++ backend/humboldt/settings/development.py
@@ -4,7 +4,8 @@ from humboldt.settings.base import *
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@(5psz&8fxu85lnz*9m4fzs4(08e+c#f0=roovhx!tdgr0*n0b'
+# SECRET_KEY = '@(5psz&8fxu85lnz*9m4fzs4(08e+c#f0=roovhx!tdgr0*n0b'
+SECRET_KEY = 'posgres'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
